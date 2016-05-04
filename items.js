cf7e9fd8417753e56012c1a33aaf9fd335e20112
Alex Shestmintsev <shestmintsev@gmail.com>
2016-05-04 22:56:53 +0300

    lab1 finished

--- items.js
+++ items.js
@@ -61,42 +61,14 @@ function ItemDAO(database) {
 
     this.getItems = function(category, page, itemsPerPage, callback) {
         "use strict";
-        page = page || 1;
-
-        /*
-         * TODO-lab1B
-         *
-         * LAB #1B: Implement the getItems() method.
-         *
-         * Create a query on the "item" collection to select only the items
-         * that should be displayed for a particular page of a given category.
-         * The category is passed as a parameter to getItems().
-         *
-         * Use sort(), skip(), and limit() and the method parameters: page and
-         * itemsPerPage to identify the appropriate products to display on each
-         * page. Pass these items to the callback function.
-         *
-         * Sort items in ascending order based on the _id field. You must use
-         * this sort to answer the final project questions correctly.
-         *
-         * Note: Since "All" is not listed as the category for any items,
-         * you will need to query the "item" collection differently for "All"
-         * than you do for other categories.
-         *
-         */
-
-        var pageItem = this.createDummyItem();
-        var pageItems = [];
-        for (var i=0; i<5; i++) {
-            pageItems.push(pageItem);
-        }
+        page = page || 0;
 
         const filter = {};
         if (category && category !== 'All') {
           filter.category = category;
         }
 
-        const skip = itemsPerPage * (page - 1);
+        const skip = itemsPerPage * page;
 
         this.db.collection('item')
           .find(filter)
@@ -109,13 +81,6 @@ function ItemDAO(database) {
             const pageItems = docs;
             callback(pageItems);
           });
-
-        // TODO-lab1B Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the items for the selected page
-        // to the callback.
-
     }
 
 
@@ -124,24 +89,17 @@ function ItemDAO(database) {
 
         var numItems = 0;
 
-        /*
-         * TODO-lab1C:
-         *
-         * LAB #1C: Implement the getNumItems method()
-         *
-         * Write a query that determines the number of items in a category
-         * and pass the count to the callback function. The count is used in
-         * the mongomart application for pagination. The category is passed
-         * as a parameter to this method.
-         *
-         * See the route handler for the root path (i.e. "/") for an example
-         * of a call to the getNumItems() method.
-         *
-         */
+        const filter = {};
+        if (category && category !== 'All') {
+          filter.category = category;
+        }
 
-         // TODO Include the following line in the appropriate
-         // place within your code to pass the count to the callback.
-        callback(numItems);
+        this.db.collection('item')
+          .find(filter)
+          .count((err, numItems) => {
+            if (err) console.error(err);
+            callback(numItems);
+          });
     }
 
 
