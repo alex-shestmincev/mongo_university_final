671e5a6f0e11a0e2a2642014f9412af799f6d47b
Alex Shestmintsev <shestmintsev@gmail.com>
2016-05-06 22:12:16 +0300

    lab3 finish

--- items.js
+++ items.js
@@ -106,88 +106,46 @@ function ItemDAO(database) {
     this.searchItems = function(query, page, itemsPerPage, callback) {
         "use strict";
 
-        /*
-         * TODO-lab2A
-         *
-         * LAB #2A: Implement searchItems()
-         *
-         * Using the value of the query parameter passed to searchItems(),
-         * perform a text search against the "item" collection.
-         *
-         * Sort the results in ascending order based on the _id field.
-         *
-         * Select only the items that should be displayed for a particular
-         * page. For example, on the first page, only the first itemsPerPage
-         * matching the query should be displayed.
-         *
-         * Use limit() and skip() and the method parameters: page and
-         * itemsPerPage to select the appropriate matching products. Pass these
-         * items to the callback function.
-         *
-         * searchItems() depends on a text index. Before implementing
-         * this method, create a SINGLE text index on title, slogan, and
-         * description. You should simply do this in the mongo shell.
-         *
-         */
+        const skip = itemsPerPage * page;
 
-        var item = this.createDummyItem();
-        var items = [];
-        for (var i=0; i<5; i++) {
-            items.push(item);
-        }
-
-        // TODO-lab2A Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the items for the selected page
-        // of search results to the callback.
-        callback(items);
+        this.db.collection('item')
+          .find({$text:{$search: query}})
+          .sort({_id:1})
+          .limit(itemsPerPage)
+          .skip(skip)
+          .toArray((err, items) => {
+            if (err) console.error(err);
+            console.log('items', items);
+            callback(items);
+          });
     }
 
 
     this.getNumSearchItems = function(query, callback) {
         "use strict";
 
-        var numItems = 0;
+        this.db.collection('item')
+          .find({$text:{$search: query}})
+          .sort({_id:1})
+          .count((err, numItems) => {
+            if (err) console.error(err);
+            console.log('numItems', numItems);
 
-        /*
-        * TODO-lab2B
-        *
-        * LAB #2B: Using the value of the query parameter passed to this
-        * method, count the number of items in the "item" collection matching
-        * a text search. Pass the count to the callback function.
-        *
-        * getNumSearchItems() depends on the same text index as searchItems().
-        * Before implementing this method, ensure that you've already created
-        * a SINGLE text index on title, slogan, and description. You should
-        * simply do this in the mongo shell.
-        */
-
-        callback(numItems);
+            callback(numItems);
+          });
     }
 
 
     this.getItem = function(itemId, callback) {
         "use strict";
 
-        /*
-         * TODO-lab3
-         *
-         * LAB #3: Implement the getItem() method.
-         *
-         * Using the itemId parameter, query the "item" collection by
-         * _id and pass the matching item to the callback function.
-         *
-         */
+        this.db.collection('item').findOne({_id: itemId})
+          .then((item) => {
+            console.log('item', item);
 
-        var item = this.createDummyItem();
-
-        // TODO-lab3 Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the matching item
-        // to the callback.
-        callback(item);
+            callback(item);
+          })
+          .catch((err) => console.log(err));
     }
 
 
